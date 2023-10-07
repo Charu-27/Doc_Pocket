5e29323d4b292e786577f3d0b30d686aab3c054f
charu jain <83021204+Charu-27@users.noreply.github.com>
2023-10-07 11:58:26 +0530

    commit changes

--- src/components/gdrive/Dashboard.js
+++ src/components/gdrive/Dashboard.js
@@ -6,13 +6,13 @@ import Folder from "./Folder"
 import File from "./File"
 import FolderBreadcrumbs from "./FolderBreadcrumbs"
 import  {useFolder}  from "../hooks/useFolder"
-import { useParams, useLocation } from "react-router-dom"
+import { useParams} from "react-router-dom"
 
 
 
 export default function Dashboard() {
   const { folderId } = useParams()
-  const { state = {} } = useLocation()
+  
   const {folder, childFolders,childFiles} = useFolder(folderId)
   console.log(childFolders)
    
@@ -20,17 +20,39 @@ export default function Dashboard() {
     <>
       <Navbar />
       <Container fluid>
+        <div>
+        <FolderBreadcrumbs currentFolder={folder}/>
+         <AddFileButton currentFolder={folder} />
           <AddFolderButton currentFolder={folder} />
+          </div>
           {childFolders.length > 0 && (
           <div className="d-flex flex-wrap">
-            {childFolders.map(childFolders => (
-              <div 
-                key={childFolders.id}
-              ><Folder folder={childFolders} />
-           </div>))}
-           </div>
+            {childFolders.map(childFolder => (
+              <div
+                key={childFolder.id}
+                style={{ maxWidth: "250px" }}
+                className="p-2"
+              >
+                <Folder folder={childFolder} />
+              </div>
+            ))}
+          </div>
         )}
-
+        {childFolders.length > 0 && childFiles.length > 0 && <hr />}
+        {childFiles.length > 0 && (
+          <div className="d-flex flex-wrap">
+            {childFiles.map(childFile => (
+              <div
+                key={childFile.id}
+                style={{ maxWidth: "250px" }}
+                className="p-2"
+              >
+                <File file={childFile} />
+              </div>
+            ))}
+          </div>
+        )}
+          
       </Container>
     </>
   )
